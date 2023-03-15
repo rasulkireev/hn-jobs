8a2d0390bc03d6d3fe1e55f8645f885aa36ad048
Rasul Kireev <rasul.kireev@guycarp.com>
2023-03-15 19:28:51 -0400

    add whitnoise

--- hackernews_developers/settings.py
+++ hackernews_developers/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,6 +139,8 @@ STATIC_ROOT = BASE_DIR.joinpath("static/")
 STATICFILES_DIRS = [
     BASE_DIR.joinpath("frontend/build"),
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 WEBPACK_LOADER = {
