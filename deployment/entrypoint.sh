302457953661eda39e3426d15f3829b534f1d795
Rasul Kireev <rasul.kireev@guycarp.com>
2023-03-15 19:57:22 -0400

    setup deployment workflow

---
+++ deployment/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python manage.py collectstatic --noinput
+python manage.py migrate
+python manage.py djstripe_sync_models
+
+gunicorn --bind 0.0.0.0:80 --workers 3 levreview.wsgi:application
