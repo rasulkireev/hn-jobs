fe931a14fc7b67c9f0868b2d5faa28eaad3af962
Rasul Kireev <rasul.kireev@guycarp.com>
2023-05-08 09:34:52 -0400

    added typecheck to for boolean values

--- jobs/utils.py
+++ jobs/utils.py
@@ -3,24 +3,23 @@ from datetime import datetime
 
 logger = logging.getLogger(__file__)
 
-def clean_job_json_object(original_comment: dict, nlp_data: dict) -> dict:
-  # nlp_data["years_of_experience_required"] = correct_years_of_experience_value(
-  #   nlp_data['years_of_experience_required'],
-  #   original_comment['text']
-  # )
-  # nlp_data["level"] = check_that_level_is_one_the_allowed_values(nlp_data['level'])
+def clean_profile_json_object(original_comment: dict, nlp_data: dict) -> dict:
+  nlp_data["years_of_experience"] = check_years_of_experience_value(nlp_data['years_of_experience'], original_comment['text'])
+  nlp_data["level"] = check_that_level_is_one_the_allowed_values(nlp_data['level'])
+  check_boolean_value(nlp_data["is_remote"])
+  check_boolean_value(nlp_data["is_onsite"])
 
   for key, value in nlp_data.items():
       nlp_data[key] = if_value_is_unknown_return_empty_string(value)
 
   return nlp_data
 
-def correct_years_of_experience_value(years: int, text: str) -> bool:
+def check_years_of_experience_value(years: int, text: str):
   """Python function to check that the estimated years of experience appears in the text."""
-  if str(years) in text:
+  if str(years) in text and isinstance(years, int):
       return years
   else:
-      return 0
+      return None
 
 def check_that_level_is_one_the_allowed_values(level: str) -> bool:
     if level in ["Senior", "Mid-level", "Junior", "Principal", "C-Level"]:
@@ -34,26 +33,6 @@ def if_value_is_unknown_return_empty_string(value: str) -> str:
     else:
         return value
 
-"""
-options for willing to relocate + mapping
-- Yes
-  - Open for relocation
-  - Anywhere really
-  - Sure!
-  - Yes, only if sponsored
-  - open to discussing it
-  - Can discuss
-- Yes (ceratin places)
-  - Within Texas
-- No
-  - Not in the next year
-- Maybe
-  - In exceptional cases
-  - It depends on the position.
-  - Possibly
-  - Possibly
-"""
-
 
 def sort_dates(dates):
     """
@@ -62,3 +41,9 @@ def sort_dates(dates):
     date_format = "%B %Y"
     sorted_dates = sorted(dates, key=lambda x: datetime.strptime(x, date_format))
     return sorted_dates
+
+def check_boolean_value(boolean_value: any) -> bool:
+    if isinstance(boolean_value, bool) or boolean_value in ["True", "true", "Yes", "yes"]:
+        return boolean_value
+    else:
+        return False
